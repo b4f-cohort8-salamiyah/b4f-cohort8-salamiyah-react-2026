308e78cdf00a5f2f72a1931166358e5c0f109b74
nawar-hisso <101182851+nawar-hisso@users.noreply.github.com>
2026-08-31 11:44:21 +0300

    :construction: Session 06 Teamwork Started

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { ChangeEvent, useState } from "react";
 import Header from "./components/Header";
 import StatCard from "./components/StatCard";
 import TaskItem from "./components/TaskItem";
 
 function App() {
+  // const currentFilter = "all";
+  const [currentFilter, setCurrentFilter] = useState("pending");
+  const [searchText, setSearchText] = useState("kjasdbfkbads");
+
+  function handleShowAll() {
+    setCurrentFilter("all");
+  }
+
+  function handleShowCompleted() {
+    setCurrentFilter("completed");
+  }
+
+  function handleShowPending() {
+    setCurrentFilter("pending");
+  }
+
+  function handleSearchChange(event: ChangeEvent<HTMLInputElement>) {
+    setSearchText(event.target.value);
+  }
+
   return (
     <div>
       <Header />
@@ -14,9 +35,24 @@ function App() {
         </section>
 
         <section className="filters">
-          <button className="filter-button active">All</button>
-          <button className="filter-button">Completed</button>
-          <button className="filter-button">Pending</button>
+          <button
+            className={`filter-button ${currentFilter === "all" ? "active" : ""}`}
+            onClick={handleShowAll}
+          >
+            All
+          </button>
+          <button
+            className={`filter-button ${currentFilter === "completed" ? "active" : ""}`}
+            onClick={handleShowCompleted}
+          >
+            Completed
+          </button>
+          <button
+            className={`filter-button ${currentFilter === "pending" ? "active" : ""}`}
+            onClick={handleShowPending}
+          >
+            Pending
+          </button>
         </section>
 
         <section className="search">
@@ -24,7 +60,13 @@ function App() {
             type="text"
             className="search-input"
             placeholder="Search tasks..."
+            value={searchText}
+            onChange={handleSearchChange}
           />
+
+          {searchText !== "" ? (
+            <p className="search-feedback">Searching for: {searchText}</p>
+          ) : null}
         </section>
 
         <ul className="task-list">
