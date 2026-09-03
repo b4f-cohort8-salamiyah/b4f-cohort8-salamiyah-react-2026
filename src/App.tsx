bab499e199aa88afa30d02b2bf82efea17abf32b
RamiAlshaar <rami.b4f@gmail.com>
2026-09-03 04:32:09 +0300

    Session 07 homework

--- src/App.tsx
+++ src/App.tsx
@@ -72,6 +72,10 @@ function App() {
     setSelectedUserId(0);
   }
 
+  function handleShowPerson(userId: number) {
+    setSelectedUserId(userId);
+  }
+
   const search = searchText.toLowerCase();
 
   const visibleTasks = tasks.filter((task) => {
@@ -195,28 +199,38 @@ function App() {
           </button>
 
           {users.map((user) => (
-            <button key={user.id} className="filter-button">
+            <button
+              key={user.id}
+              className={
+                "filter-button" + (selectedUserId === user.id ? " active" : "")
+              }
+              onClick={() => handleShowPerson(user.id)}
+            >
               {user.name}
             </button>
           ))}
         </section>
 
-        <ul className="task-list">
-          {visibleTasks.map((task) => {
-            const statusText = task.completed ? "Completed" : "Pending";
-            const statusClass = task.completed ? "completed" : "pending";
+        {visibleTasks.length === 0 ? (
+          <p className="wmpty-state">No tasks to show.</p>
+        ) : (
+          <ul className="task-list">
+            {visibleTasks.map((task) => {
+              const statusText = task.completed ? "Completed" : "Pending";
+              const statusClass = task.completed ? "completed" : "pending";
 
-            return (
-              <TaskItem
-                key={task.id}
-                title={task.title}
-                ownerName={getOwnerName(task.userId)}
-                statusText={statusText}
-                statusClass={statusClass}
-              />
-            );
-          })}
-        </ul>
+              return (
+                <TaskItem
+                  key={task.id}
+                  title={task.title}
+                  ownerName={getOwnerName(task.userId)}
+                  statusText={statusText}
+                  statusClass={statusClass}
+                />
+              );
+            })}
+          </ul>
+        )}
 
         <p className="visible-count">
           {visibleTasks.length} of {tasks.length} tasks shown
